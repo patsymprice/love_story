7668fcc2ebf964589eb34200521f224e614a7d6c
Patsy Price <patsy.m.price@gmail.com>
2013-06-25 15:01:20 -0700

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LoveStory::Application.config.secret_token = '9fcbc244e0dd10f60e2601811993eac4310bfaaeb6199c940feac589e3dfe3ceb4e6fd822ecd996052ae746a09298a777981cc266c15d335fd055cb43a799db6'
